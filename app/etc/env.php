a4b8613a186476a8d824486fae54b93ed3f0ad3a
AhmedMakhlooqDev <76881779+AhmedMakhlooqDev@users.noreply.github.com>
2023-09-16 19:36:30 +0300

    Upload

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_1260yh'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'ptHktLYRgA6Rn1OeLAWgg5ZU0lPRdork'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'd19_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'd19_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '7c102118f03969fff4b2198adfbaf6a7'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magentoTest',
+                'username' => 'ahmed',
+                'password' => 'admin123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'makhlooq.magento.com'
+    ],
+    'install' => [
+        'date' => 'Sat, 16 Sep 2023 12:51:33 +0000'
+    ]
+];
